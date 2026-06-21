f620a9ce8b86e3bd1687fabe50fec90242146d73
Pranav Nakka <pranavnakka15@gmail.com>
2026-06-21 19:45:56 +0530

    Add a guessing game that prompts user for input

---
+++ Guessing Game.py
@@ -0,0 +1,16 @@
+import random
+jackpot = random.randint(1,100)
+guess = int(input("Enter:"))
+count = 1
+
+while guess != jackpot:
+    if guess < jackpot:
+        print("Guess Higher")
+    else:
+        print("Guess Lower")
+    
+    guess = int(input("Enter:"))
+    count += 1
+
+print("Correct Answer!")
+print("You took",count,"attempts to guess.")
